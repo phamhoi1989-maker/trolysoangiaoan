0b7a13ff5446fa7afb4bb981e263cb6d36fe07e9
phamhoi1989-maker <phamhoi1989@gmail.com>
2025-12-10 10:49:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Phạm Văn Hồi - Trường Tiểu học Thiện Kế - ĐT: 0975 787 864</p>
 </div>
 """, unsafe_allow_html=True)
 
